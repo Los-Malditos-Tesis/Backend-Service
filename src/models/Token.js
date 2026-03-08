615b434c57ec4bb0e17bdc94bc50167ef7f01c61
Fabio-Alfredo <00210121@uca.edu.sv>
2026-03-07 18:51:10 -0600

    :sparkles: feat: add new models

---
+++ src/models/Token.js
@@ -0,0 +1,35 @@
+import { DataTypes } from "sequelize";
+
+export default (sequelize)=>{
+    const Token = sequelize.define("Token", {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true
+        },
+        content:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate:{
+                notEmpty: true
+            }
+        },
+        isValid:{
+            type: DataTypes.BOOLEAN,
+            defaultValue:true
+        }
+    },{
+        tableName: "tokens",
+        underscored: true,
+        timestamps: true,
+        paranoid: true,
+    })
+
+    Token.associate = (models)=>{
+        Token.belongsTo(models.User, {
+            foreignKey: "userId"
+        })
+    }
+
+    return Token
+}
---
+++ src/models/User.js
@@ -0,0 +1,84 @@
+import { DataTypes } from "sequelize";
+import bcrypt from "bcryptjs";
+
+export default (sequelize) => {
+    const User = sequelize.define("User", {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                len: [3, 30],
+                is: /^[a-zA-Z0-9._-]+$/
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: {
+                args: true,
+                msg: 'Email already in user!'
+            },
+            validate: {
+                isEmail: {
+                    msg: 'Must be a valid email address'
+                },
+                notEmpty: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        status: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true
+        }
+
+    }, {
+        tableName: "users",
+        underscored: true,
+        timestamps: true,
+        paranoid: true,
+
+        hooks: {
+            beforeCreate: async (user) => {
+                if (user.password) {
+                    user.password = await bcrypt.hash(user.password, 10)
+                }
+            },
+
+            beforeUpdate: async (user) => {
+                if (user.change("password")) {
+                    user.password = await bcrypt.hash(user.password, 10)
+                }
+            }
+        }
+    })
+
+    User.prototype.validatePassword = async function (password) {
+        return bcrypt.compare(password, this.password)
+    }
+
+    User.associate = (models) => {
+        User.belongsToMany(models.Role, {
+            through: "user_role",
+            foreignKey: "userId",
+            otherKey: "roleId"
+        });
+
+        User.hasMany(models.Token, {
+            foreignKey: "userId"
+        })
+    };
+
+    return User;
+}
