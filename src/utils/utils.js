5365fc0b98afd02e0617c5e918815371dcbfcf0d
Fabio-Alfredo <00210121@uca.edu.sv>
2026-03-28 20:59:30 -0600

    :sparkles: feat: add new envs

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,8 @@
 import { config } from "../config/config.js";
 
 export const generateTime = () => {
-    const time = new Date().toLocaleString("sv-SE", {
-        timeZone: "America/El_Salvador",
+    const time = new Date().toLocaleString(config.appLocal, {
+        timeZone: config.appTimeZone,
         hour12: false,
     }).replace(" ", "T")
 
