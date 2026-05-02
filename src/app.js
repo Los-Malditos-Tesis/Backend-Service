53b16b7f928253e5caf74c0577c813842bcf6096
Fabio-Alfredo <00210121@uca.edu.sv>
2026-05-01 22:42:18 -0600

    :sparkles: feat: add cors config

--- src/app.js
+++ src/app.js
@@ -1,22 +1,24 @@
-import express from 'express'
-import syncDatabase from '../src/libs/database/sync.sequelize.js'
-import { contextMiddleware } from './middlewares/context_middleware.js'
-import { globalErrorHandler } from './errors/global_error_handler.js'
-import { config } from './config/config.js'
-import router from './route/index.js'
+import express from "express";
+import syncDatabase from "../src/libs/database/sync.sequelize.js";
+import { contextMiddleware } from "./middlewares/context_middleware.js";
+import { globalErrorHandler } from "./errors/global_error_handler.js";
+import { config } from "./config/config.js";
+import router from "./route/index.js";
+import cors from "cors";
 
-const app = express()
-syncDatabase()
+const app = express();
+syncDatabase();
 
-app.use(express.json())
-app.use(contextMiddleware)
-app.use(config.basePath, router)
-app.use(globalErrorHandler)
+app.use(cors());
+app.use(express.json());
+app.use(contextMiddleware);
+app.use(config.basePath, router);
+app.use(globalErrorHandler);
 
 const runTests = async () => {
-    const mockCtx = { context: "test" };
-}
+  const mockCtx = { context: "test" };
+};
 
-runTests()
+runTests();
 
 export default app;
