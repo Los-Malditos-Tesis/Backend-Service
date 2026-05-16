91f722134f1fd7d5c6adb611db59024172b69144
HenryLima07 <00033721@uca.edu.sv>
2026-05-16 12:37:26 -0600

    update automation service

--- src/utils/gs1_util.js
+++ src/utils/gs1_util.js
@@ -32,14 +32,14 @@ export const parseGS1 = (gs1Code = "") => {
     // serial could be alfa numeric only used for box (to add on tesis TODO )
     const serialMatch = gs1Code.match(/\(21\)([\w\d]+)/);
 
-        if (ssccMatch) {
-            decodedGs1.sscc = ssccMatch[1];
-            decodedGs1.code = ssccMatch[1];
-            decodedGs1.unit_type = ITEM_TYPES.PALLET;
-            decodedGs1.gtin = gtinMatch ? gtinMatch[1] : null;
-        } else if (gtinMatch) {
-            decodedGs1.gtin = gtinMatch[1];
-            decodedGs1.unit_type = ITEM_TYPES.BOX;
+    if (ssccMatch) {
+      decodedGs1.sscc = ssccMatch[1];
+      decodedGs1.code = ssccMatch[1];
+      decodedGs1.unit_type = ITEM_TYPES.PALLET;
+      decodedGs1.gtin = gtinMatch ? gtinMatch[1] : null;
+    } else if (gtinMatch) {
+      decodedGs1.gtin = gtinMatch[1];
+      decodedGs1.unit_type = ITEM_TYPES.BOX;
 
       if (serialMatch) {
         decodedGs1.code = serialMatch[1];
