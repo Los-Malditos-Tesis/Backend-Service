f6c89ab2c14cb803a4a0a6ed3cfb166aa2268cd9
Fabio-Alfredo <00210121@uca.edu.sv>
2026-06-07 21:11:15 -0600

    :bug: fix: solution bug in search products

--- src/utils/validator/automation_validator.js
+++ src/utils/validator/automation_validator.js
@@ -2,15 +2,42 @@ import { body } from "express-validator";
 import { VALIDATION_MESSAGES as MSG } from "../const/messages.js";
 
 export const merchandiseValidator = [
-    body("gs1Code")
-        .exists()
-        .withMessage(MSG.REQUIRED.es("El código GS1"))
-        .bail()
-        .isString()
-        .withMessage(MSG.STRING.es("El código GS1"))
-        .bail()
-        .trim()
-        .isLength({ min: 15, max: 75 })
-        .withMessage(MSG.LENGTH.es("El código GS1", 15, 75))
-        .bail()
-]
+  body("gs1Code")
+    .exists()
+    .withMessage(MSG.REQUIRED.es("El código GS1"))
+    .bail()
+    .isString()
+    .withMessage(MSG.STRING.es("El código GS1"))
+    .bail()
+    .trim()
+    .isLength({ min: 15, max: 75 })
+    .withMessage(MSG.LENGTH.es("El código GS1", 15, 75))
+    .bail(),
+];
+
+export const searchProductValidator = [
+  body("searchLevel")
+    .exists()
+    .withMessage(MSG.REQUIRED.es("El nivel de búsqueda"))
+    .bail()
+    .isString()
+    .withMessage(MSG.STRING.es("El nivel de búsqueda"))
+    .bail()
+    .trim()
+    .bail(),
+  body("category")
+    .exists()
+    .withMessage(MSG.REQUIRED.es("La categoría"))
+    .bail()
+    .isString()
+    .withMessage(MSG.STRING.es("La categoría"))
+    .bail()
+    .trim()
+    .bail(),
+  body("productId")
+    .exists()
+    .withMessage(MSG.REQUIRED.es("El ID del producto"))
+    .bail()
+    .trim()
+    .bail(),
+];
